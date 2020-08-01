3a8131f27b69aedd7e4ace11ce5b4bf06bced88e
Ryan Warner <warner.ryan@gmail.com>
2020-07-31 20:24:11 -0400

    Full path for meta images

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -5,26 +5,30 @@ const siteMetadata = {
   description: 'An opinionated starting point for authoring interactive content using MDX and NextJS',
   twitterHandle: '@RyanWarnerCodes',
   favicon: '/favicon.png',
-  ogImage: '/ogImage.png'
+  ogImage: '/ogImage.png',
+  url: 'https://next-mdx.warner.codes'
 }
 
 export default function SEO({
   title,
   description,
-  ogImage,
   twitterHandle,
-  favicon
+  favicon,
+  ogImage,
+  url
 }) {
   return (
     <Head>
       <title>{title}</title>
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
-      <meta property='og:image' content={ogImage} />
+      <meta property='og:image' content={`${url}${ogImage}`} />
+      <meta propert='og:image:width' content='1200' />
+      <meta propert='og:image:height' content='630' />
       <meta name='twitter:site' content={twitterHandle} />
       <meta name='twitter:card' content='summary_large_image' />
       <link rel='icon' href={favicon} />
-      <meta name='twitter:image' content={ogImage} />
+      <meta name='twitter:image' content={`${url}${ogImage}`} />
       <link href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap' rel='stylesheet' />
     </Head>
   )
