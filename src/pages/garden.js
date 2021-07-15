ef4618acc6c27391bce77d9adc3622928ba12389
Ryan Warner <warner.ryan@gmail.com>
2021-07-14 22:09:47 -0400

    Placeholder Garden page

---
+++ src/pages/garden.js
@@ -0,0 +1,14 @@
+import { SEO, Header } from 'components'
+
+const Garden = () => {
+  return (
+    <div>
+      <SEO />
+      <Header />
+      <h1>Garden</h1>
+      <p>This is where we should put a list of our garden posts.</p>
+    </div>
+  )
+}
+
+export default Garden
