a9e1eecbececc6ffd45d01b28d4c3ef2d1502c64
Ryan Warner <warner.ryan@gmail.com>
2021-07-14 22:04:16 -0400

    Update and install deps, building successfully

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/
 })
@@ -7,32 +5,5 @@ const withMDX = require('@next/mdx')({
 module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, withMDX({
     pageExtensions: ['js', 'jsx', 'mdx'],
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      const { assetPrefix = '', urlLoaderOptions } = nextConfig
-
-      defaultLoaders.assetsImport = {
-        loader: 'url-loader',
-        options: Object.assign(
-          {},
-          {
-            emitFile: !isServer,
-            limit: 8192,
-            name: `static/[path][${dev ? "name" : "hash"}].[ext]`,
-            publicPath: `${assetPrefix}/_next/`
-          },
-          urlLoaderOptions
-        )
-      }
-
-      config.module.rules.push({
-        test: /\.(bmp|gif|jpe?g|png|svg|ttf|eot|woff2?|mp3|wav|mp4|ogg|webm)$/,
-        use: defaultLoaders.assetsImport
-      })
-
-      config.plugins.push(new webpack.IgnorePlugin(/.\/*.code-workspace/))
-
-      return config
-    }
-  })
-  )
+  }))
 }
